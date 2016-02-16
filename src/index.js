cbb39463ce1701e7f90dd0b895d60788b0b085bf
Adam Miskiewicz <adam@sk3vy.com>
2016-02-16 17:02:47 -0500

    updating Koa and exposing schema.json (for easy download by clients)

--- src/index.js
+++ src/index.js
@@ -1,25 +1,35 @@
-import koa from 'koa';
+import Koa from 'koa';
+import koaRouter from 'koa-router';
+import convert from 'koa-convert';
 import mount from 'koa-mount';
 import graphqlHTTP from 'koa-graphql';
 
 import Schema from 'Schema';
 import DataLoaders from 'DataLoaders';
 
-const app = koa();
+import { graphql } from 'graphql';
+import { introspectionQuery } from 'graphql/utilities';
 
-app.use(
-  mount(
-    '/graphql',
-    graphqlHTTP((request, context) => ({
-      schema: Schema,
-      rootValue: {
-        session: context.session,
-        loaders: DataLoaders.create(context),
-      },
-      graphiql: true,
-    }))
-  )
-);
+const app = new Koa();
+const router = koaRouter();
+
+router.all('/graphql', convert(graphqlHTTP((ctx, req) => ({
+  schema: Schema,
+  rootValue: {
+    session: ctx.session,
+    loaders: DataLoaders.create(ctx),
+  },
+  graphiql: true,
+}))));
+
+router.get('/schema.json', async (ctx, next) => {
+  const result = await graphql(Schema, introspectionQuery);
+  ctx.body = JSON.stringify(result, null, 2);
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 app.listen(3000, () => console.log(
   `GraphQL Server is now running on http://localhost:3000/`
